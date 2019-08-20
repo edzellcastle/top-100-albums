03d9d200e6d722c93f6465fe84ba3c617e221ad9
David Lindsay <davidlindsay100@icloud.com>
2019-08-20 11:26:39 -0700

    Initial Commit

---
+++ Top 100 Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top 100 Albums
+//
+//  Created by David Lindsay on 8/20/19.
+//  Copyright © 2019 Tapinfuse, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top 100 AlbumsTests/Top_100_AlbumsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_100_AlbumsTests.swift
+//  Top 100 AlbumsTests
+//
+//  Created by David Lindsay on 8/20/19.
+//  Copyright © 2019 Tapinfuse, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Top_100_Albums
+
+class Top_100_AlbumsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
